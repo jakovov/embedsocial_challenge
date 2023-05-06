a364189daef2334e37557872f9c9639e22193eff
Atanas Jakovov <atanasejakovov485@gmail.com>
2023-05-06 16:01:13 +0200

    x

--- setup/index.js
+++ setup/index.js
@@ -125,25 +125,6 @@ fetch("../data.json")
                     </div>
                 </div>
             </div>`
-
-
-
-            
-
-
-
-
-
-
-
-            // div.innerHTML =
-            //     '<div class="card-container"><div class="card-padding"><div class="card-top"><div class="profile"><div class="profile_image"><img src="' +
-            //     obj.profile_image + '" alt=""></div><div class="profile-info"><h4 class="namestyle">' +
-            //     obj.name + '</h4><p class="datestyle">' +
-            //     obj.date.split(" ", 1) + '</p></div></div><div class="topicon"><img class="" src="../icons/instagram-logo.svg"/></div></div><div class="card-img"><img class="card__image" src="' +
-            //     obj.image + '"/></div><div class="card__inner"><p class="innerstyle" style="height:150px">'+
-            //     obj.caption + '</p><hr class="u-no-margin--bottom"><div class="card__inner2"><svg class="likebtn" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.7617 3.26543C14.3999 2.90347 13.9703 2.61634 13.4976 2.42045C13.0248 2.22455 12.518 2.12372 12.0063 2.12372C11.4945 2.12372 10.9878 2.22455 10.515 2.42045C10.0422 2.61634 9.61263 2.90347 9.25085 3.26543L8.50001 4.01626L7.74918 3.26543C7.0184 2.53465 6.02725 2.1241 4.99376 2.1241C3.96028 2.1241 2.96913 2.53465 2.23835 3.26543C1.50756 3.99621 1.09702 4.98736 1.09702 6.02084C1.09702 7.05433 1.50756 8.04548 2.23835 8.77626L2.98918 9.52709L8.50001 15.0379L14.0108 9.52709L14.7617 8.77626C15.1236 8.41448 15.4108 7.98492 15.6067 7.51214C15.8026 7.03935 15.9034 6.53261 15.9034 6.02084C15.9034 5.50908 15.8026 5.00233 15.6067 4.52955C15.4108 4.05677 15.1236 3.62721 14.7617 3.26543V3.26543Z" stroke="black" stroke-linecap="round" stroke-linejoin="round"/></svg><p class="likesvalue"style="padding-left:5px">' +
-            //     obj.likes + '</p></div></div></div></div>'
             cardeden.appendChild(div);
             return div;
         }
@@ -356,18 +337,10 @@ fetch("../data.json")
                 }
                 else {
                     LightTheme();
-                    console.log("ne rabote");
+                    console.log("ne rabote.");
                 }
             });
         }
-        // var testirame = document.getElementsByClassName("grid-item");
-        // var maincontainer = document.getElementsByClassName("container");
-        // var previewcontainer = document.getElementsByClassName("preview");
-        // var sidebarcontainer = document.getElementsByClassName("sidebar");
-        // var cardcontainer = document.getElementsByClassName("card-container");
-        // var layout = document.getElementsByClassName("preview");
-        // var profilename = document.getElementsByClassName("namestyle");
-        // var cardholder = document.getElementsByClassName("post");
         let MediaQuery_mobile = window.matchMedia('(max-width: 767px)');
 
         function widthChangeCallback(MediaQuery_mobile) {
@@ -416,35 +389,5 @@ fetch("../data.json")
           MediaQuery_laptop.addEventListener('change', laptopChangeCallback);
           
           laptopChangeCallback(MediaQuery_laptop);
-        // var c = window.matchMedia("(max-width: 768px)");
-        // mobile(c);
-        // function mobile(c) {
-
-        //     previewstyle.setAttribute("style", "grid-template-columns : auto");
-        //     mainpreview[0].setAttribute("style", "width : 25%");
-        //     // for (i = 0; i < innerstyle.length; i++) {
-        //     //     innerstyle[i].setAttribute("style", "height : 250px");
-
-
-        //     // }
-        // }
-        // c.addListener(mobile);
-        
-        
-        // var sidebar = document.getElementsByClassName("sidebar");
-        // var z = window.matchMedia("(max-width: 992px)");
-        // tablet(z);
-        // function tablet(z) {
-        //     sidebar[0].setAttribute("style", "width : 30%");
-        //     previewstyle.setAttribute("style", "grid-template-columns : auto auto");
-        //     mainpreview[0].setAttribute("style", "width : 50%");
-        //     // for (i = 0; i < innerstyle.length; i++) {
-        //     //     innerstyle[i].setAttribute("style", "height : 500px");
-        //     // }
-        // }
-        // z.addListener(tablet);
-        // var x = window.matchMedia("(min-width: 992px)");
-        // show_hide(x) // Call listener function at run time
-        // x.addListener(show_hide);
     });
 
